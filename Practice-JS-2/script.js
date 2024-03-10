ddeb6cc32692aca0db0d4d2f24d4fd261614559e
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-10 17:14:51 +0530

    updated

---
+++ Practice-JS-2/script.js
@@ -0,0 +1,20 @@
+//VARIABLES
+//"use strict" // makes keyword mandatory to use
+b=5;
+console.log(b);
+
+
+var a=5; // old method
+console.log(a);
+
+let a1=5;  //good practice to use let & const
+const a2=5;
+console.log(a1);
+console.log(a2);
+
+let a3= "Hello";
+console.log(a3);
+
+const a4="Hello World"
+const a5=a4;
+console.log(a5);
