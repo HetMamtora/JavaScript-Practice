76fce260c6c020c4effec64497be6d24a20db42f
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-11 21:33:02 +0530

    Update script.js

--- Practice-JS-4/script.js
+++ Practice-JS-4/script.js
@@ -94,3 +94,79 @@ console.log(arr_set);
 
 const s=new Set([10,20,30,40,50,30]); //takes only unique value - no repeated value
 console.log(s);
+s.add(60);
+s.add("Hello");
+console.log(s);
+for(let element of s){
+    length++;
+}
+console.log(length);
+
+
+console.log("");
+
+//MAP [DATA STRUCTURE - MAP]
+console.log("MAP:");
+
+console.log("Map-1:");
+const map1=new Map([[1,"One"],
+    ["fname","Mickey"],
+    ["whole number",[0,1,2,3,4]]
+])
+console.log(map1.get(1));
+console.log(map1);
+
+console.log("Map-2:");
+const map2=new Map();
+map2.set("fname","Mouse");
+console.log(map2);
+
+
+console.log("");
+
+//THIS KEYWORD
+console.log("THIS KEYWORD:");
+
+const obj_this={
+    fname:"Thomas",
+    age:30,
+    funthis:function(){
+        console.log(this.fname); // this is refering to obj_this
+    }
+}
+obj_this.funthis();
+
+
+console.log("");
+
+//NEW KEYWORD
+console.log("NEW KEYWORD:");
+
+function fun_new(){
+    let fName="John";
+    this.fName=fName;
+}
+
+const obj_new=new fun_new();
+console.log(obj_new.fName);
+
+
+console.log("");
+
+//CONSTRUCTORS
+//Constructor Functions technically are regular functions.
+//There are 2 conventions though:
+//1- they are names with capital letter first.
+//2- They should be executed only with "new" operator.
+
+console.log("CONSTRUCTORS:");
+function User(name){
+    this.name=name;
+}
+console.log(new User("Thomas"));
+
+
+console.log("");
+
+//SYMBOL
+console.log("SYMBOL:");
