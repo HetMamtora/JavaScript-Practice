3ba980bca871edd2c348e66941c3862882065210
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-12 18:35:44 +0530

    Update script.js

--- Practice-JS-5/script.js
+++ Practice-JS-5/script.js
@@ -1,17 +1,39 @@
 //ASYNCHRONOUS
+//JS is Synchronous
 console.log("ASYNCHRONOUS:");
+//SET-TIMEOUT
+console.log("Set-Timeout:");
+/*setTimeout(()=> {
+    console.log("Asynchronous");
+},1000);*/
 
+setTimeout(fun,0);
+console.log("This");
 
-console.log("");
+function fun(){
+    console.log("Asynchronous");
+}
+console.log("is");
+
+//console.log("");
 
 //SET TIMEOUT
-console.log("SET TIMEOUT:");
+//console.log("SET TIMEOUT:");
 
 
 console.log("");
 
 //CALL-BACK
 console.log("CALL-BACK:");
+
+function fun1(val){
+    console.log(val);
+}
+function add(a,b,callback){
+    const sum=a+b;
+    callback(sum);
+}
+add(5,10,fun1)
 
 
 console.log("");
@@ -20,13 +42,19 @@ console.log("");
 console.log("CALL-BACK HELL:");
 
 
-console.log("");
+loadingData();
+collectingData();
+approvingData();
+approved();
+
+
+//console.log("");
 
 //PROMISE
-console.log("PROMISE:");
+//console.log("PROMISE:");
 
 
-console.log("");
+//console.log("");
 
 //ASYNC_AWAIT
-console.log("ASYNC_AWAIT:");
+//console.log("ASYNC_AWAIT:");
