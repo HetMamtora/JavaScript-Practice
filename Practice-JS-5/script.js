5e25a2a671c03cb29419eec122a212e273559a38
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-12 18:27:36 +0530

    updated

---
+++ Practice-JS-5/script.js
@@ -0,0 +1,32 @@
+//ASYNCHRONOUS
+console.log("ASYNCHRONOUS:");
+
+
+console.log("");
+
+//SET TIMEOUT
+console.log("SET TIMEOUT:");
+
+
+console.log("");
+
+//CALL-BACK
+console.log("CALL-BACK:");
+
+
+console.log("");
+
+//CALL-BACK HELL
+console.log("CALL-BACK HELL:");
+
+
+console.log("");
+
+//PROMISE
+console.log("PROMISE:");
+
+
+console.log("");
+
+//ASYNC_AWAIT
+console.log("ASYNC_AWAIT:");
