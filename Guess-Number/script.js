fe23aeaacc13e53d8c3671aeb5a31ca86b6e349c
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-13 21:51:41 +0530

    updated

--- Guess-Number/script.js
+++ Guess-Number/script.js
@@ -1,20 +1,50 @@
-const input = document.getElementById('input');
-const btn = document.getElementById("btn");
-const wrng = document.querySelector('.wrng');
-const guesses = document.getElementById("guesses")
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let wrng = document.querySelector('.wrng');
+let guesses = document.getElementById("guesses");
 
-const answer = Math.floor(Math.random()*100)+1;
-const numGuesses = 0;
+let answer = Math.floor(Math.random() * 100) + 1;
+let numGuesses = 0;
 
-btn.addEventListener("click",()=>{
-    guessesNumber();
+btn.addEventListener("click", () => {
+  guessNumber();
 });
 
-function guessesNumber(){
-    if(input.value<1 || input.value>100 || isNaN(input.value)){
-        wrng.innerHTML = "Enter between 1 to 100."
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    guessNumber();
+  }
+});
+
+function guessNumber() {
+  if (input.value < 1 || input.value > 100 || isNaN(input.value)) {
+    wrng.innerHTML = "Enter Between 1 to 100";
+  } else {
+    numGuesses++;
+    guesses.innerHTML = "No. of Guess : " + numGuesses;
+    if (input.value > answer) {
+      wrng.innerHTML = "You guessed too high!";
+      input.value="";
+    } else if (input.value < answer) {
+      wrng.innerHTML = "You guessed too low!";
+      input.value="";
+    } else {
+      wrng.innerHTML = "Congratulations! You guessed the correct number in " + numGuesses + " guesses!";
+      btn.disabled = true;
+      setTimeout(()=>{
+        resetGame();
+      },5000)
+
     }
-    else{
-        numGuesses++;
-    }
+  }
 }
+
+function resetGame() {
+    numGuesses = 0;
+    answer = Math.floor(Math.random() * 100) + 1;
+    input.value = "";
+    wrng.innerHTML = "";
+    btn.disabled = false;
+    guesses.innerHTML = "No. of Guess : 0";
+  }
