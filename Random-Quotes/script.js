0cd41ee6028d9d4674085fcebdb71843452edf74
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-13 16:46:53 +0530

    update

---
+++ Random-Quotes/script.js
@@ -0,0 +1,12 @@
+const btn =  document.getElementById("btn");
+const output = document.querySelector(".output");
+
+const quote = [
+            "The only place where success comes before work is in the dictionary",
+            "Strive not to be a success, but rather to be of value",
+            "Try not to become a man of success, Rather become a man of value",
+            "Success is stumbling from failure to failure with no loss of enthusiasm",
+            "Don't spend time beating on a wall, hoping to transform it into a door.",
+            "It is hard to fail, but it is worse never to have tried to succeed.",
+            "If you want to live a happy life, tie it to a goal, not to people or things"
+];
